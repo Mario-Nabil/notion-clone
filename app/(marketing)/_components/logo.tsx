e192dd89f0d697e09beaeadb058743f9ad9cc638
Mario-Nabil <scorpion_nabil@hotmail.com>
2024-02-14 19:44:32 -0500

    corregir hidden en el logo cuando es version movile

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export const Logo = () => {
   return (
-    <div className="hiden md:flex items-center gap-x-2">
+    <div className="hidden md:flex items-center gap-x-2">
       <Image
         src="/logo.svg"
         height={"40"}
